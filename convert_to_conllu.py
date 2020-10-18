eadfe13f64d3060be0e3d5f6d5cb44004a47ebe9
Roshan Ram <roshan.ram0101@gmail.com>
2020-10-18 10:39:31 -0400

    convert str to int

--- convert_to_conllu.py
+++ convert_to_conllu.py
@@ -11,7 +11,7 @@ with codecs.open(args.input, 'r', encoding='utf-8') as fin, codecs.open(args.out
         info = line.strip().split()
         for token_num, token in enumerate(info):
             data = ["_" for _ in range(10)]
-            data[0] = token_num
+            data[0] = str(token_num)
             data[1] = token
             data[2] = token
             data[3] = 'PUNCT'
